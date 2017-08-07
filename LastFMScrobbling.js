626caac5f32fb8592fe2a42e6346e75c18b78afa
NT5 <hendelporras@hotmail.com>
2017-08-07 15:44:49 -0600

    Added LastFMScrobbling.js

---
+++ LastFMScrobbling.js
@@ -0,0 +1,298 @@
+registerPlugin({
+    name: 'LastFM Scrobbling',
+    version: '0.0.1',
+    engine: '>= 0.9.18',
+    description: 'Scrobbling Tracks to LastFM',
+    author: 'NT5',
+    vars: [
+        {
+            name: 'lsfm_username',
+            title: 'LastFM Username',
+            placeholder: 'The last.fm username or email address.',
+            type: 'string'
+        },
+        {
+            name: 'lsfm_password',
+            title: 'LastFM password in plain text',
+            type: 'password'
+        }
+    ]
+}, function (sinusbot, config) {
+
+    var engine = require('engine');
+    var event = require('event');
+    var store = require('store');
+    var helpers = require('helpers');
+
+    // String format util
+    if (!String.prototype.format) {
+        String.prototype.format = function () {
+            var str = this.toString();
+            if (!arguments.length) {
+                return str;
+            }
+            var args = typeof arguments[0];
+            args = (("string" === args || "number" === args) ? arguments : arguments[0]);
+            for (var arg in args) {
+                str = str.replace(RegExp("\\{" + arg + "\\}", "gi"), args[arg]);
+            }
+            return str;
+        }
+    }
+
+    // URL format util http://stackoverflow.com/questions/1714786/
+    function URLSerialize (obj, prefix) {
+        var str = [], p;
+        for (p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+                str.push((v !== null && typeof v === "object") ?
+                         URLSerialize(v, k) :
+                         encodeURIComponent(k) + "=" + encodeURIComponent(v));
+            }
+        }
+        return str.join("&");
+    }
+
+    // Polyfill util https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+    if (typeof Object.assign !== 'function') {
+        Object.assign = function (target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target === null) { // TypeError if undefined or null
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+
+                if (nextSource !== null) { // Skip over if undefined or null
+                    for (var nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        };
+    }
+
+    var app = {
+        util: {
+            get_timestamp: function () {
+                return Math.floor(Date.now() / 1000);
+            }
+        },
+        config: {
+            lastfm: {
+                username: config.lsfm_username || '',
+                password: config.lsfm_password || ''
+            },
+            plugin: {
+                manifest: {
+                    version: '0.0.1',
+                    authors: [
+                        {
+                            name: 'NT5',
+                            role: 'Main Dev'
+                        }
+                    ]
+                }
+            },
+            api: {
+                lastfm: {
+                    url: 'https://ws.audioscrobbler.com/2.0/?{data}',
+                    api_key: '6485c66027a4fc738ff0175028cead10',
+                    api_secret: 'bccc1c85b8e99b5e97fc5bd3829655a9'
+                }
+            }
+        },
+        getJSON: function (options) {
+            options = (typeof options !== "object") ? {} : options;
+
+            options.method = options.method || 'GET';
+            options.url = options.url || '';
+            options.headers = options.headers || { 'Content-Type': 'application/json; charset=UTF-8' };
+            options.callback = options.callback || function (err, res) { engine.log(res); };
+            options.error_callback = options.error_callback || function (err, res) { engine.log(err); };
+
+            /*
+             TODO
+              - [ENH] Port to new script engine
+            */
+            sinusbot.http({
+                method: options.method,
+                url: options.url,
+                headers: options.headers
+            }, function (err, res) {
+                if (err || res.statusCode !== 200) {
+                    engine.log('Request error [{error}] Code: [{code}] Data: [{data}]'.format({
+                        error: err,
+                        data: res.data,
+                        code: res.statusCode
+                    }));
+                    options.error_callback(err, res);
+                } else {
+                    var json = JSON.parse(res.data);
+                    options.callback(json);
+                }
+            });
+        },
+        api: {
+            lastfm: {
+                signature_call: function (data) {
+                    var api_secret = app.config.api.lastfm.api_secret || '0';
+                    var api_sig = [];
+
+                    Object.keys(data).forEach(function (key) {
+                        var value = data[key];
+                        api_sig.push(key + "" + value);
+                    });
+                    api_sig.push(api_secret);
+                    return helpers.MD5Sum(api_sig.join(''));
+                },
+                postData: function (data, callback) {
+                    var url =  app.config.api.lastfm.url;
+
+                    var post_data = Object.assign(data, {
+                        api_sig: app.api.lastfm.signature_call(data),
+                        format: 'json'
+                    });
+
+                    app.getJSON({
+                        method: 'POST',
+                        url: url.format({
+                            data: URLSerialize(post_data)
+                        }),
+                        callback: callback,
+                        error_callback: function (err, res) {
+                            var json = JSON.parse(res.data);
+                            if (json.error === 9) {
+                                app.callbacks.lastfm.store_login();
+                            }
+                        }
+                    });
+                },
+                getMobileSession: function (username, password, callback) {
+                    var api_key = app.config.api.lastfm.api_key;
+
+                    app.api.lastfm.postData({
+                        api_key: api_key,
+                        method: 'auth.getMobileSession',
+                        password: password,
+                        username: username
+                    }, callback);
+
+                },
+                getSession: function (token, callback) {
+                    var api_key = app.config.api.lastfm.api_key;
+
+                    app.api.lastfm.postData({
+                        api_key: api_key,
+                        method: 'auth.getSession',
+                        token: token
+                    }, callback);
+                },
+                updateNowPlaying: function (artist, title) {
+                    var api_key = app.config.api.lastfm.api_key,
+                        client_token = store.get('lastfm_client_token');
+
+                    app.api.lastfm.postData({
+                        api_key: api_key,
+                        artist: artist,
+                        method: 'track.updateNowPlaying',
+                        sk: client_token,
+                        track: title
+                    });
+
+                    engine.log("Scroblling Now Playing: {track} | {artist}".format({
+                        artist: artist,
+                        track: title
+                    }));
+                },
+                scrobble: function (artist, title) {
+                    var api_key = app.config.api.lastfm.api_key,
+                        client_token = store.get('lastfm_client_token');
+
+                    app.api.lastfm.postData({
+                        api_key: api_key,
+                        artist: artist,
+                        chosenByUser: '0',
+                        method: 'track.scrobble',
+                        sk: client_token,
+                        timestamp: app.util.get_timestamp(),
+                        track: title
+                    });
+
+                    engine.log("Scroblling: {track} | {artist}".format({
+                        artist: artist,
+                        track: title
+                    }));
+                }
+            }
+        },
+        callbacks: {
+            lastfm: {
+                store_login: function () {
+                    var username = app.config.lastfm.username,
+                        password = app.config.lastfm.password;
+
+                    store.set('lastfm_client_token', '0');
+                    store.set('lastfm_username', username);
+                    store.set('lastfm_password', password);
+
+                    app.api.lastfm.getMobileSession(username, password, function (json) {
+                        var token = json.session.key;
+                        store.set('lastfm_client_token', token);
+                        engine.log("Login as {username}".format({
+                            username: username
+                        }));
+                    });
+                }
+            }
+        }
+    };
+
+    // Check for script version & loggin
+    (function () {
+        var version = store.get('script_version')
+
+        // Version reset
+        if (version !== app.config.plugin.manifest.version) {
+            engine.log('Your running a different version of the script, resetting configuration, please reconfigure it from web panel.');
+            engine.notify('Configure LastFM script');
+
+            store.set('script_version', app.config.plugin.manifest.version);
+            engine.saveConfig({});
+            store.unset('lastfm_client_token');
+            store.unset('lastfm_username');
+            store.unset('lastfm_password');
+        }
+
+        // LastFM loggin
+        var username = app.config.lastfm.username,
+            password = app.config.lastfm.password;
+
+        if (!store.get('lastfm_client_token') || (username !== store.get('lastfm_username') || password !== store.get('lastfm_password'))) {
+            app.callbacks.lastfm.store_login();
+        }
+
+    }());
+
+    event.on('trackInfo', function (track) {
+        var artist = track.tempArtist() || track.artist();
+        var title = track.tempTitle() || track.title();
+
+        if (title && artist) {
+            app.api.lastfm.updateNowPlaying(artist, title);
+        }
+    });
+
+    event.on('trackEnd', function (track) {
+        app.api.lastfm.scrobble(track.artist(), track.title());
+    });
+
+});
